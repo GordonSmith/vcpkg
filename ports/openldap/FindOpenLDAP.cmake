7fc84964a5dcf323c05a48be9b28e9955b0f7a06
Gordon Smith <GordonJSmith@gmail.com>
2022-08-02 11:11:03 +0100

    [openlap] Add new port
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

---
+++ ports/openldap/FindOpenLDAP.cmake
@@ -0,0 +1,17 @@
+# - Find OpenLDAP C Libraries
+#
+# OPENLDAP_FOUND - True if found.
+# OPENLDAP_INCLUDE_DIR - Path to the openldap include directory
+# OPENLDAP_LIBRARIES - Paths to the ldap and lber libraries
+
+find_path(OPENLDAP_INCLUDE_DIR ldap.h)
+
+find_library(LDAP_LIBRARY ldap)
+find_library(LBER_LIBRARY lber)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OpenLDAP DEFAULT_MSG OPENLDAP_INCLUDE_DIR LDAP_LIBRARY LBER_LIBRARY)
+
+set(OPENLDAP_LIBRARIES ${LDAP_LIBRARY} ${LBER_LIBRARY})
+
+mark_as_advanced(LDAP_LIBRARY LBER_LIBRARY)
---
+++ ports/openldap/portfile.cmake
@@ -0,0 +1,40 @@
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.5.13.tgz"
+    FILENAME "openldap-2.5.13.tgz"
+    SHA512 30fdc884b513c53169910eec377c2ad05013b9f06bab3123d50d028108b24548791f7f47f18bcb3a2b4868edeab02c10d81ffa320c02d7b562f2e8f2fa25d6c9
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    PATCHES
+        openssl.patch
+)
+
+execute_process(
+    COMMAND autoreconf
+    WORKING_DIRECTORY ${SOURCE_PATH}
+)
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        --disable-slapd
+        --with-tls=openssl
+        --without-cyrus-sasl
+        "LIBS=-ldl"
+)
+
+vcpkg_build_make(BUILD_TARGET depend LOGFILE_ROOT depend)
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/var")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/var")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/FindOpenLDAP.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/openldap" RENAME copyright)
